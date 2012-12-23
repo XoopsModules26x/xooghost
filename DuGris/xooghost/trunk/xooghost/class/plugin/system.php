2d4de66a61539dae095e9691f2641dd07b0cc607
DuGris <DuGris@4a504592-4a38-4d1b-8683-cdd87551f2be>
2012-12-23 01:16:02 +0000

    Plugin system (users post)

---
+++ DuGris/xooghost/trunk/xooghost/class/plugin/system.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Xooghost module
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @package         Xooghost
+ * @since           2.6.0
+ * @author          Laurent JEN (Aka DuGris)
+ * @version         $Id$
+ */
+
+class XooghostSystemPlugin extends Xoops_Plugin_Abstract implements SystemPluginInterface
+{
+    public function UserSync($uid)
+    {        $ghost_module = Xooghost::getInstance();
+        $ghost_handler = $ghost_module->GhostHandler();
+
+        $criteria = new CriteriaCompo();
+        $criteria->add( new Criteria('xooghost_online', 1) ) ;
+        $criteria->add( new Criteria('xooghost_published', time(), '<=') ) ;
+        $criteria->add( new Criteria('xooghost_uid', $uid) );
+
+        return $ghost_handler->getCount($criteria);
+    }
+}
